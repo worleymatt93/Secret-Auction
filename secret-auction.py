f9d3964cb17dd90300ed6137d0c7e5eef781e43e
worleymatt93 <138177023+worleymatt93@users.noreply.github.com>
2025-04-07 17:13:40 -0400

    Add files via upload

---
+++ secret-auction.py
@@ -0,0 +1,44 @@
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+import art
+
+
+# print("hello")
+# print("\n" * 100)
+# print("Type something")
+
+print(art.logo)
+print("Welcome to the secret auction.")
+
+
+def get_top_bid(bidding_dictionary):
+    highest_bid = 0
+    highest_bidder = ""
+
+    for bidder in auction_dictionary:
+
+        if auction_dictionary[bidder] > highest_bid:
+            highest_bid = auction_dictionary[bidder]
+            highest_bidder = bidder
+
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}!")
+
+
+auction_dictionary = {}
+auction_running = True
+while auction_running:
+
+    user_name = input("What is your name? ")
+    user_bid = int(input("What is your bid? $"))
+
+    auction_dictionary[user_name] = user_bid
+
+    keep_going = input("Are there any other bidders? ('Yes' or 'No')\n").lower()
+    if keep_going == "no":
+        auction_running = False
+    print("\n" * 20)
+
+get_top_bid(auction_dictionary)
